5a10d768a9841ff2700c9635a085e6d739f867de
Brandon Barksdale <bbark93@gmail.com>
2024-05-24 17:36:23 -0400

    added try catch to delete

--- api/server.js
+++ api/server.js
@@ -1,80 +1,88 @@
 // BUILD YOUR SERVER HERE
-const express = require('express');
-const User = require('./users/model')
-const server = express()
-server.use(express.json())
+const express = require("express");
+const User = require("./users/model");
+const server = express();
+server.use(express.json());
 
-server.delete('/api/users/:id', async (req, res) => {
-    const possibleUser = await User.findById(req.params.id)
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
     if (!possibleUser) {
-        res.status(404).json({
-            message: "The user with the specified ID does not exist"
-        })
-    }else {
-        const deletedUser = await User.remove(possibleUser.id)
-        res.status(200).json(deletedUser)
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      message: "error deleting user",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
 
-server.post('/api/users', (req, res) => {
-    const user = req.body;
-    console.log(user);
-    if (!user.name || !user.bio) {
-        res.status(400).json({
-            message: "Please provide name and bio for the user"
-        })
-    }else {
-      User.insert(user)  
-        .then(createdUser => {
-            res.status(201).json(createdUser)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error creating user',
-                err: err.message,
-                stack: err.stack,
-            })
-        })
-    }
-})
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
 
-server.get('/api/users', (req, res) => {
-    User.find()
-        .then(users => {
-            res.json(users)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error getting users',
-                err: err.message,
-                stack: err.stack,
-            })
-        })
-})
-
-server.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            if (!user) {
-                res.status(404).json({
-                    message: "The user with the specified ID does not exist",
-                })
-            }
-            res.json(user)
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: 'error getting user',
-                err: err.message,
-                stack: err.stack,
-            })
-        })
-})
-
-server.use('*', (req, res) => {
-    res.status(404).json({
-        message: 'not found'
+server.get("/api/users", (req, res) => {
+  User.find()
+    .then((users) => {
+      res.json(users);
     })
-})
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
 
-module.exports = server // EXPORT YOUR SERVER instead of {}
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({
+          message: "The user with the specified ID does not exist",
+        });
+      }
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting user",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
